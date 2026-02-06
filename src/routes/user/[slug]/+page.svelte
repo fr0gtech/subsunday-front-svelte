ac684266dccee14ba95b2af7d44db6b899c03e1b
GREEB <gr44bli@gmail.com>
2026-02-06 22:55:56 +0100

    fix: cleanup user page

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -48,99 +48,101 @@
 <div
 	class="mx-auto flex w-full max-w-screen-xl grow flex-wrap justify-center gap-5 space-y-5 p-5 pt-16 leading-relaxed lg:pt-16"
 >
-	<Card class="flex h-fit gap-2 p-3 lg:w-1/2">
-		<div class="flex">
-			<div class="flex items-center gap-2">
-				<div class="relative h-20 w-20 overflow-clip rounded-full bg-neutral-800">
-					{#if data.user.streak < 5}
-						<FadeInImage alt="fire and sparks" src="/particles/leaves.gif" class={cn('absolute')} />
-					{/if}
-					{#if data.user.streak > 0 && data.user.streak < 31}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/fire_sparks.gif"
-							class={cn(
-								'absolute z-10 grayscale',
-								data.user.streak > 5 && 'grayscale-50',
-								data.user.streak > 10 && 'saturate-150',
-								data.user.streak > 15 && 'hue-rotate-180',
-								data.user.streak > 20 && 'hue-rotate-60',
-								data.user.streak > 20 && 'hue-rotate-180'
-							)}
-						/>
-					{/if}
+	<Card class="flex h-fit gap-2 overflow-clip p-3 lg:w-1/2">
+		<div class="flex items-center gap-2">
+			<div class="relative mr-2 h-20 w-25 overflow-clip rounded-full bg-neutral-800">
+				{#if data.user.streak < 5}
+					<FadeInImage alt="fire and sparks" src="/particles/leaves.gif" class={cn('absolute')} />
+				{/if}
+				{#if data.user.streak > 0 && data.user.streak < 31}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/fire_sparks.gif"
+						class={cn(
+							'absolute z-10 grayscale',
+							data.user.streak > 5 && 'grayscale-50',
+							data.user.streak > 10 && 'saturate-150',
+							data.user.streak > 15 && 'hue-rotate-180',
+							data.user.streak > 20 && 'hue-rotate-60',
+							data.user.streak > 20 && 'hue-rotate-180'
+						)}
+					/>
+				{/if}
 
-					{#if data.user.streak > 30 && data.user.streak < 50}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/flamethrower.gif"
-							class="absolute bottom-0 z-1 scale-150 -rotate-[90deg] hue-rotate-180"
-						/>
-					{/if}
-					{#if data.user.streak > 35 && data.user.streak < 50}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/flamethrower.gif"
-							class="absolute bottom-0 z-1 scale-150 -rotate-[45deg] hue-rotate-180"
-						/>
-					{/if}
-					{#if data.user.streak > 40 && data.user.streak < 50}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/flamethrower.gif"
-							class="absolute bottom-0 z-1 scale-150 rotate-[230deg] hue-rotate-180"
-						/>
-					{/if}
-					{#if data.user.streak > 50 && data.user.streak < 60}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/gravity.gif"
-							class="absolute bottom-0 z-0 scale-100 rotate-[230deg] -hue-rotate-60"
-						/>
-					{/if}
-					{#if data.user.streak > 55}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/spark1.gif"
-							class="absolute top-0 z-0 scale-100 rotate-[10deg] -hue-rotate-60"
-						/>
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/spark1.gif"
-							class="absolute bottom-0 left-0 z-0 scale-200 -rotate-[10deg] -hue-rotate-60"
-						/>
-					{/if}
-					{#if data.user.streak > 65}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/spark1.gif"
-							class="absolute top-0 z-0 scale-150 rotate-[80deg] -hue-rotate-60"
-						/>
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/spark1.gif"
-							class="absolute bottom-0 -left-1 z-0 scale-200 -rotate-[70deg] -hue-rotate-60"
-						/>
-					{/if}
-					{#if data.user.streak > 75}
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/spark1.gif"
-							class="absolute top-0 z-0 scale-150 rotate-[120deg] -hue-rotate-60"
-						/>
-						<FadeInImage
-							alt="fire and sparks"
-							src="/particles/spark1.gif"
-							class="absolute bottom-0 -left-1 z-0 scale-200 -rotate-[130deg] -hue-rotate-60"
-						/>
-					{/if}
-				</div>
-				<div class="font-mono! text-xs">
+				{#if data.user.streak > 30 && data.user.streak < 50}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/flamethrower.gif"
+						class="absolute bottom-0 z-1 scale-150 -rotate-[90deg] hue-rotate-180"
+					/>
+				{/if}
+				{#if data.user.streak > 35 && data.user.streak < 50}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/flamethrower.gif"
+						class="absolute bottom-0 z-1 scale-150 -rotate-[45deg] hue-rotate-180"
+					/>
+				{/if}
+				{#if data.user.streak > 40 && data.user.streak < 50}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/flamethrower.gif"
+						class="absolute bottom-0 z-1 scale-150 rotate-[230deg] hue-rotate-180"
+					/>
+				{/if}
+				{#if data.user.streak > 50 && data.user.streak < 60}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/gravity.gif"
+						class="absolute bottom-0 z-0 scale-100 rotate-[230deg] -hue-rotate-60"
+					/>
+				{/if}
+				{#if data.user.streak > 55}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/spark1.gif"
+						class="absolute top-0 z-0 scale-100 rotate-[10deg] -hue-rotate-60"
+					/>
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/spark1.gif"
+						class="absolute bottom-0 left-0 z-0 scale-200 -rotate-[10deg] -hue-rotate-60"
+					/>
+				{/if}
+				{#if data.user.streak > 65}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/spark1.gif"
+						class="absolute top-0 z-0 scale-150 rotate-[80deg] -hue-rotate-60"
+					/>
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/spark1.gif"
+						class="absolute bottom-0 -left-1 z-0 scale-200 -rotate-[70deg] -hue-rotate-60"
+					/>
+				{/if}
+				{#if data.user.streak > 75}
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/spark1.gif"
+						class="absolute top-0 z-0 scale-150 rotate-[120deg] -hue-rotate-60"
+					/>
+					<FadeInImage
+						alt="fire and sparks"
+						src="/particles/spark1.gif"
+						class="absolute bottom-0 -left-1 z-0 scale-200 -rotate-[130deg] -hue-rotate-60"
+					/>
+				{/if}
+			</div>
+			<div class="flex w-full justify-between gap-5">
+				<div class="flex items-center">
 					<a href={`https://twitch.tv/${data.user.name}`}>
-						<h2 class="text-lg font-bold">{data.user.name}</h2>
+						<h2 class="text-2xl font-bold">{data.user.name}</h2>
 					</a>
-					<span
-						>votes<Badge variant="secondary"
+				</div>
+				<div class="flex flex-col justify-end space-y-3 font-mono! text-xs">
+					<span class="text-right text-sm"
+						>votes: <Badge class="text-sm!" variant="secondary"
 							>{data.user.votes.length}
 							{#if parseInt(data.user.voteRank) === 0}
 								<span class="text-amber-400">(#{parseInt(data.user.voteRank) + 1})</span>
@@ -149,12 +151,12 @@
 							{:else if parseInt(data.user.voteRank) === 2}
 								<span class="text-gray-400">(#{parseInt(data.user.voteRank) + 1})</span>
 							{:else}
-								<span>(#{parseInt(data.user.voteRank) + 1})</span>
+								<span class="text-xs!">(#{parseInt(data.user.voteRank) + 1})</span>
 							{/if}
 						</Badge></span
 					>
-					<span
-						>streak<Badge variant="secondary"
+					<span class="text-right"
+						>streak: <Badge class="text-sm!" variant="secondary"
 							>{data.user.streak}
 							{#if parseInt(data.user.streakRank) === 0}
 								<span class="text-amber-400">(#{parseInt(data.user.streakRank) + 1})</span>
@@ -163,7 +165,7 @@
 							{:else if parseInt(data.user.streakRank) === 2}
 								<span class="text-gray-400">(#{parseInt(data.user.streakRank) + 1})</span>
 							{:else}
-								<span>(#{parseInt(data.user.streakRank) + 1})</span>
+								<span class="text-xs!">(#{parseInt(data.user.streakRank) + 1})</span>
 							{/if}
 						</Badge>
 					</span>
